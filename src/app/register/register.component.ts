2a9c38b571451340868623286040b727c66b9d01
vivek683 <73192110+vivek683@users.noreply.github.com>
2022-06-12 18:47:31 +0530

    form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -9,37 +9,44 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  uname=""
-  acno=""
-  pswd=""
-
+  
   // form group
-  registerForm=this.fb.group({
-    acno:'',
-    pswd:'',
-    uname:''
+  registerForm = this.fb.group({
+
+    acno: [''],
+    pswd: [''],
+    uname: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]]
+  
   });
 
-  constructor(private ds:DataService,private router:Router,private fb:FormBuilder) { }
+  constructor(private ds: DataService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
-  register(){
-   var uname = this.uname;
-   var acno = this.acno;
-   var pswd = this.pswd;
+  register() {
 
-   const result = this.ds.register(uname,acno,pswd);
+    var uname = this.registerForm.value.uname;
+    var acno = this.registerForm.value.acno;
+    var pswd = this.registerForm.value.pswd;
+    console.log(this.registerForm.valid);
 
-   if(result){
-     alert("Successfully registered");
-   this.router.navigateByUrl("");
+    if(this.registerForm.valid){
+      const result = this.ds.register(uname, acno, pswd);
+
+    if (result) {
+      alert("Successfully registered");
+      this.router.navigateByUrl("");
     }
-else{
-  alert("Already Existing Customer....Please Log In!!!")
-}
-  
+    else {
+      alert("Already Existing Customer....Please Log In!!!")
+   
+    }
+  }
+  else{
+    alert("Invalid Form");
+  }
 
-}
+
+  }
 }
